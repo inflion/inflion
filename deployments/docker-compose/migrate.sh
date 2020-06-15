bd10e6675a14f02dce97b9641b6f421dfb96878d
fuubit <46063265+fuubit@users.noreply.github.com>
2020-06-15 13:27:17 +0900

    abolish-hasura

--- deployments/docker-compose/migrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --net host --rm \
-  -v "$(pwd)/../../dbconfig.yml:/workspace/dbconfig.yml" \
-  -v "$(pwd)/../../internal/store/schema:/workspace/internal/store/schema" \
-  pyar6329/sql-migrate:latest up
--- internal/timescale/schema/metrics.sql
+++ internal/timescale/schema/metrics.sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.metrics (
+CREATE TABLE metrics (
     "time" timestamp with time zone NOT NULL,
     instance_id character varying(255) NOT NULL,
     type character varying(255) NOT NULL,
@@ -6,5 +6,3 @@ CREATE TABLE public.metrics (
 );
 CREATE INDEX metrics_time_idx ON public.metrics USING btree ("time" DESC);
 CREATE INDEX metrics_type_time_idx ON public.metrics USING btree (type, "time" DESC);
-
-SELECT create_hypertable('metrics', 'time');
