d0580eaa7276116cbf5ea56c0e51ce6cc5091d66
fuubit <46063265+fuubit@users.noreply.github.com>
2020-07-22 11:28:20 +0900

    Enabled to notify config event

--- internal/ops/flow/flow_test.go
+++ internal/ops/flow/flow_test.go
@@ -10,11 +10,43 @@
 
 package flow
 
-import "testing"
+import (
+	"encoding/json"
+	"github.com/inflion/inflion/internal/ops/monitor"
+	"io/ioutil"
+	"testing"
+)
 
 func TestFlow(t *testing.T) {
-	f := NewOpsFlow(MockRecipeReader{})
-	_, err := f.Run(NewExecutionContext())
+	jsonForTest := "event.json"
+
+	bytes, err := ioutil.ReadFile(jsonForTest)
+	if err != nil {
+		t.Error(err)
+	}
+
+	event := monitor.MonitoringEvent{}
+
+	err = json.Unmarshal(bytes, &event)
+	if err != nil {
+		t.Error(err)
+	}
+
+	event.RawEventBody = bytes
+
+	ec := NewExecutionContext()
+	ec.AddFields("project", ExecutionFields{
+		Values: map[string]interface{}{"id": event.Project},
+	})
+	ec.AddFields("event", ExecutionFields{
+		Values: event.EventBody,
+	})
+	ec.AddFields("raw-event", ExecutionFields{
+		Values: map[string]interface{}{"json": event.RawEventBody},
+	})
+
+	flow := NewOpsFlow(MockRecipeReader{})
+	_, err = flow.Run(ec)
 	if err != nil {
 		t.Error(err)
 	}
